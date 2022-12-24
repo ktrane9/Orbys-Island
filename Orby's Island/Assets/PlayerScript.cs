b175d48513b6bc7ddf37e6af6e370ae55d3dbcdb
Ktrane <43693004+ktrane9@users.noreply.github.com>
2022-12-24 01:27:28 -0500

    Directional Orb Spawn (#3)

--- Orby's Island/Assets/PlayerScript.cs
+++ Orby's Island/Assets/PlayerScript.cs
@@ -4,20 +4,24 @@ using UnityEngine;
 //using UnityEngine.Tilemaps;
 
 public class PlayerScript : MonoBehaviour {
+    [SerializeField] GameObject orb;
+
     private Animator animator;
     private Vector2 movement;
     //private Rigidbody2D body;
     public float speed;
+    public Vector3 lastDirection;
 
     void Start() {
         //body = GetComponent<Rigidbody2D>();
+        lastDirection = -transform.up;
     
-        
     }
 
     // Update is called once per frame
     void Update() {
         move();
+        attack();
         
     }
 
@@ -29,11 +33,13 @@ public class PlayerScript : MonoBehaviour {
         if(Input.GetKey("down")) {
           //animator.SetInteger("Motion", walk_down); //Play Walk Down Animation
             transform.Translate(movement * speed * Time.deltaTime);
+            lastDirection = -transform.up;
         }
 
         else if(Input.GetKey("up")) {
           //animator.SetInteger("Motion", walk_up); //Play Walk Up Animation
             transform.Translate(movement * speed * Time.deltaTime);
+            lastDirection = transform.up;
         }
         
         else if(Input.GetKey("right")) {
@@ -43,6 +49,7 @@ public class PlayerScript : MonoBehaviour {
         //   }
         //   animator.SetInteger("Motion", walk_side); //Play Walk Side Animation
             transform.Translate(movement * speed * Time.deltaTime);
+            lastDirection = transform.right;
         }
 
         else if(Input.GetKey("left")) {
@@ -52,10 +59,19 @@ public class PlayerScript : MonoBehaviour {
         //   }
         //   animator.SetInteger("Motion", walk_side); //Play Walk Side Animation Flipped
             transform.Translate(movement * speed * Time.deltaTime);
+            lastDirection = -transform.right;
         }
 
         // else {
         //   animator.SetInteger("Motion", idle); //Play Idle Animation
         // }
+    }
+
+    private void attack() {
+         if (Input.GetKeyDown("space")) {
+            float spawnDis = 1.0f;
+            Vector2 spawnPos = transform.position + (lastDirection * spawnDis);
+            Instantiate(orb, spawnPos, Quaternion.identity);
+        }
     }
 }
