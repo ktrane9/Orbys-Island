d6157d021a485a17aa0356e7aea45dde5aab6658
Ktrane <43693004+ktrane9@users.noreply.github.com>
2022-12-25 04:45:34 -0500

    Orb Given Square Collision (#4)

--- Orby's Island/Assets/PlayerScript.cs
+++ Orby's Island/Assets/PlayerScript.cs
@@ -78,4 +78,10 @@ public class PlayerScript : MonoBehaviour {
            
         }
     }
+
+    void OnCollisionEnter2D(Collision2D collision){
+        if(collision.gameObject.tag == "Orb") {
+            if(Input.GetKey("z")) Orb.Spawn.setKick();
+        }
+    }
 }
