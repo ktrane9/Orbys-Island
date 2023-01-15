71fdd844319b12c2a83b054b55dde910510d5ca4
Ktrane <ktrane9@gmail.com>
2023-01-15 00:47:35 -0500

    Player Movement Updated

--- Orby's Island/Assets/PlayerScript.cs
+++ Orby's Island/Assets/PlayerScript.cs
@@ -8,7 +8,7 @@ public class PlayerScript : MonoBehaviour {
 
     private Animator animator;
     private Vector2 movement;
-    //private Rigidbody2D body;
+    public Rigidbody2D body;
     public float speed;
     public Vector3 lastDirection;
 
@@ -19,29 +19,39 @@ public class PlayerScript : MonoBehaviour {
     }
 
     void Update() { //Processing Inputs
+        //processInput();
         move();
         attack();
         
     }
 
     void FixedUpdate() { //Physics Calculations
+        
+    }
+
+    void processInput() {
+        float moveX = Input.GetAxisRaw("Horizontal"); // Raw = Vector movement only 0 or 1
+        float moveY = Input.GetAxisRaw("Vertical");
+        movement = new Vector2(moveX, moveY);
 
     }
 
     private void move() {
-        movement = new Vector2(0,0);
-        movement.x = Input.GetAxis("Horizontal");
-        movement.y = Input.GetAxis("Vertical");
-
+        float moveX = Input.GetAxisRaw("Horizontal"); // Raw = Vector movement only 0 or 1
+        float moveY = Input.GetAxisRaw("Vertical");
+        movement = new Vector2(moveX, moveY);
+        
         if(Input.GetKey("down")) {
           //animator.SetInteger("Motion", walk_down); //Play Walk Down Animation
-            transform.Translate(movement * speed * Time.deltaTime);
+            // body.velocity = new Vector2(movement.x * speed, movement.y * speed).normalized;
+            transform.Translate(movement.normalized * speed * Time.deltaTime);
             lastDirection = -transform.up;
         }
 
         else if(Input.GetKey("up")) {
-          //animator.SetInteger("Motion", walk_up); //Play Walk Up Animation
-            transform.Translate(movement * speed * Time.deltaTime);
+            //animator.SetInteger("Motion", walk_up); //Play Walk Up Animation
+            // body.velocity = new Vector2(movement.x * speed, movement.y * speed).normalized;
+            transform.Translate(movement.normalized * speed * Time.deltaTime);
             lastDirection = transform.up;
         }
         
@@ -50,8 +60,9 @@ public class PlayerScript : MonoBehaviour {
         //     facingRight = false;
         //     transform.Rotate(new Vector2(0, 180));
         //   }
-        //   animator.SetInteger("Motion", walk_side); //Play Walk Side Animation
-            transform.Translate(movement * speed * Time.deltaTime);
+        //  animator.SetInteger("Motion", walk_side); //Play Walk Side Animation
+            // body.velocity = new Vector2(movement.x * speed, movement.y * speed).normalized;
+            transform.Translate(movement.normalized * speed * Time.deltaTime);
             lastDirection = transform.right;
         }
 
@@ -60,8 +71,9 @@ public class PlayerScript : MonoBehaviour {
         //     facingRight = true;
         //     transform.Rotate(new Vector2(0, 180));
         //   }
-        //   animator.SetInteger("Motion", walk_side); //Play Walk Side Animation Flipped
-            transform.Translate(movement * speed * Time.deltaTime);
+        //  animator.SetInteger("Motion", walk_side); //Play Walk Side Animation Flipped
+            // body.velocity = new Vector2(movement.x * speed, movement.y * speed).normalized;
+            transform.Translate(movement.normalized * speed * Time.deltaTime);
             lastDirection = -transform.right;
         }
 
